317f12eb433052c2d8f4b1e13de4874919626a6f
EstebanCode91 <estebancode91@gmail.com>
2025-01-27 21:26:56 -0400

    last changes

--- src/service/index.ts
+++ src/service/index.ts
@@ -4,9 +4,13 @@ interface Response {
     data: Product[]
 }
 
+import data from '../lib/data.json'
+
 export async function getProductsFilteredByTitle (): Promise<Response> {
-    const response = await fetch('http://localhost:3000/api')
-    const { data } = await response.json()
+    // const response = await fetch('http://localhost:3000/api')
+    // const { data } = await response.json()
+
+
 
     return data
 }
